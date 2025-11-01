e3cdd884426afec67e72bcfca032c2232bb10f28
Gustavo <gustavoiensue@edu.unifil.br>
2025-10-31 21:28:43 -0300

    Membro 1: Implementa funcionalidade de soma no Main.py

---
+++ Main.py
@@ -0,0 +1,12 @@
+# Arquivo: Main.py
+# Autor: Gustavo Henrique Iensue de Olieviragit add Main.py
+# Feature: feature-soma
+
+print("--- Algoritmo de Soma ---")
+
+valor1 = float(input("Digite o primeiro valor: "))
+valor2 = float(input("Digite o segundo valor: "))
+
+soma = valor1 + valor2
+
+print(f"O resultado da soma é: {soma}")
